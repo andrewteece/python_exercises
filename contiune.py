1b685a31c033ff92c28cbe5487a42973715d946d
Andrew Teece <andrewteece@gmail.com>
2016-04-04 09:54:44 -0500

    loops

---
+++ contiune.py
@@ -0,0 +1,9 @@
+while True:
+    line = raw_input('>')
+    if line [0] == '#':
+        continue
+    if line == 'done':
+        break
+    print line
+print 'Done!'
+
---
+++ simple_definite_loop_2.py
@@ -0,0 +1,7 @@
+friends = ['Joseph', 'Glenn', 'Sally']
+
+for friend in friends:
+    print 'Happy New Year:', friend
+    
+print 'Done!'
+
