afd29ca67f36995c03200d3a1a0982579c54b7dd
Andrew Teece <andrewteece@gmail.com>
2016-04-06 16:20:27 -0500

    more ideas

--- exercise_5_2.py
+++ exercise_5_2.py
@@ -1,24 +1,23 @@
 largest = None
 smallest = None
 while True:
-    u_inp = raw_input("Enter a number: ")
-    if u_inp == "done":break
+    u_inp = raw_input('Enter a number: ')
+    if u_inp == 'done':
+        break
     try:
         num = int(u_inp)
-    except: 
-        print "Invalid input"
+    except:
+        print 'Invalid input'
         continue
-    else: 
-        if smallest is None: 
-            smallest = num
-            largest = num
-        elif num < smallest:
-            smallest = num
-        elif num > largest:
-            largest = num
 
-print "Maximum is", largest
-print "Minimum is", smallest
+    if largest is None or num > largest:
+        largest = num
+
+    if smallest is None or num < smallest:
+        smallest = num
+
+print 'Maximum is', largest
+print 'Minimum is', smallest
 
 
 
